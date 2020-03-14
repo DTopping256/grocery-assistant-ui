72276de8511250548ee6eab0905e1478616efaee
Daniel Topping <DTopping256@gmail.com>
2020-03-14 11:17:45 +0000

    Created CRA project but cleaned up a bit with: ESLint setup, typescript, jest unit tests and PWA stuff.

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './App.css';
+
+/**
+ * The component representing the entire single page application, which is rendered inside the root element of the project template.
+ */
+export default class App extends React.Component {
+	public render () {
+		return <div className="App"></div>;
+	}
+}
